f9e0cae1fc270474513aa2f04cbea31921343f21
dan <dan@buuna.com>
2014-02-13 15:52:57 +1100

    Initial commit

---
+++ BNToolkit/BNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNAppDelegate.h
+//  BNToolkit
+//
+//  Created by Daniel Rodrigues on 13/02/2014.
+//  Copyright (c) 2014 Buuna Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BNToolkit/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  BNToolkit
+//
+//  Created by Daniel Rodrigues on 13/02/2014.
+//  Copyright (c) 2014 Buuna Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNAppDelegate class]));
+    }
+
+}
